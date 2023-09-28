89020d4e93ecf94fe42391736a80272a0cd77a77
karovgose <karovgose7@gmail.com>
2023-09-28 10:14:37 +0200

    add toggle button and update css

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,30 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const closeNav = () => {
+    setIsNavOpen(false);
+  };
+
   return (
-    <nav className="navigation">
-      <ul className="navigation-list">
+    <nav className={`navigation ${isNavOpen ? "open" : ""}`}>
+      <button className="toggle-button" onClick={toggleNav}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </button>
+      <ul className={`navigation-list ${isNavOpen ? "open" : ""}`}>
         <li className="navigation-item">
-          <Link to="/Counter" className="navigation-link">
+          <Link to="/Counter" className="navigation-link" onClick={closeNav}>
             Counter
           </Link>
         </li>
         <li className="navigation-item">
-          <Link to="/Todo" className="navigation-link">
+          <Link to="/Todo" className="navigation-link" onClick={closeNav}>
             To Do List
           </Link>
         </li>
-
         <li className="navigation-item">
-          <Link to="/PostList" className="navigation-link">
+          <Link to="/PostList" className="navigation-link" onClick={closeNav}>
             Post List
           </Link>
         </li>
         <li className="navigation-item">
-          <Link to="/CurrencyConverter" className="navigation-link">
-            Currency Convertor
+          <Link
+            to="/CurrencyConverter"
+            className="navigation-link"
+            onClick={closeNav}
+          >
+            Currency Converter
           </Link>
         </li>
       </ul>
