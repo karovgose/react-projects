cd75cd76da5acb169d90534b4e5d98e42286c018
karovgose <karovgose7@gmail.com>
2023-09-29 00:05:08 +0200

    added option more option for currency to convert

--- src/CurrencyConvertor.js
+++ src/CurrencyConvertor.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./CurrencyConverter.css"; // Import your CSS file
+import "./CurrencyConverter.css";
 
 export default function CurrencyConverter() {
   const [amount, setAmount] = useState(1);
@@ -7,6 +7,7 @@ export default function CurrencyConverter() {
   const [toCur, setToCur] = useState("USD");
   const [exchangeRates, setExchangeRates] = useState(null);
   const [converted, setConverted] = useState(null);
+  const [currencyOptions, setCurrencyOptions] = useState([]);
 
   useEffect(() => {
     fetch(
@@ -15,6 +16,10 @@ export default function CurrencyConverter() {
       .then((res) => res.json())
       .then((data) => {
         setExchangeRates(data.conversion_rates);
+
+        // Extract currency options from the API response
+        const options = Object.keys(data.conversion_rates);
+        setCurrencyOptions(options);
       });
   }, [fromCur]);
 
@@ -22,7 +27,7 @@ export default function CurrencyConverter() {
     if (exchangeRates) {
       const rate = exchangeRates[toCur];
       const result = amount * rate;
-      setConverted(result);
+      setConverted(result.toFixed(2));
     }
   }, [amount, fromCur, toCur, exchangeRates]);
 
@@ -47,58 +52,11 @@ export default function CurrencyConverter() {
           onChange={(e) => setFromCur(e.target.value)}
           className="currency-select"
         >
-          <option value="USD">USD</option>
-          <option value="AED">AED</option>
-          <option value="ARS">ARS</option>
-          <option value="AUD">AUD</option>
-          <option value="BGN">BGN</option>
-          <option value="BRL">BRL</option>
-          <option value="BSD">BSD</option>
-          <option value="CAD">CAD</option>
-          <option value="CHF">CHF</option>
-          <option value="CLP">CLP</option>
-          <option value="CNY">CNY</option>
-          <option value="COP">COP</option>
-          <option value="CZK">CZK</option>
-          <option value="DKK">DKK</option>
-          <option value="DOP">DOP</option>
-          <option value="EGP">EGP</option>
-          <option value="EUR">EUR</option>
-          <option value="FJD">FJD</option>
-          <option value="GBP">GBP</option>
-          <option value="GTQ">GTQ</option>
-          <option value="HKD">HKD</option>
-          <option value="HRK">HRK</option>
-          <option value="HUF">HUF</option>
-          <option value="IDR">IDR</option>
-          <option value="ILS">ILS</option>
-          <option value="INR">INR</option>
-          <option value="ISK">ISK</option>
-          <option value="JPY">JPY</option>
-          <option value="KRW">KRW</option>
-          <option value="KZT">KZT</option>
-          <option value="MVR">MVR</option>
-          <option value="MXN">MXN</option>
-          <option value="MYR">MYR</option>
-          <option value="NOK">NOK</option>
-          <option value="NZD">NZD</option>
-          <option value="PAB">PAB</option>
-          <option value="PEN">PEN</option>
-          <option value="PHP">PHP</option>
-          <option value="PKR">PKR</option>
-          <option value="PLN">PLN</option>
-          <option value="PYG">PYG</option>
-          <option value="RON">RON</option>
-          <option value="RUB">RUB</option>
-          <option value="SAR">SAR</option>
-          <option value="SEK">SEK</option>
-          <option value="SGD">SGD</option>
-          <option value="THB">THB</option>
-          <option value="TRY">TRY</option>
-          <option value="TWD">TWD</option>
-          <option value="UAH">UAH</option>
-          <option value="UYU">UYU</option>
-          <option value="ZAR">ZAR</option>
+          {currencyOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
         </select>
 
         <select
@@ -107,58 +65,11 @@ export default function CurrencyConverter() {
           onChange={(e) => setToCur(e.target.value)}
           className="currency-select"
         >
-          <option value="USD">USD</option>
-          <option value="AED">AED</option>
-          <option value="ARS">ARS</option>
-          <option value="AUD">AUD</option>
-          <option value="BGN">BGN</option>
-          <option value="BRL">BRL</option>
-          <option value="BSD">BSD</option>
-          <option value="CAD">CAD</option>
-          <option value="CHF">CHF</option>
-          <option value="CLP">CLP</option>
-          <option value="CNY">CNY</option>
-          <option value="COP">COP</option>
-          <option value="CZK">CZK</option>
-          <option value="DKK">DKK</option>
-          <option value="DOP">DOP</option>
-          <option value="EGP">EGP</option>
-          <option value="EUR">EUR</option>
-          <option value="FJD">FJD</option>
-          <option value="GBP">GBP</option>
-          <option value="GTQ">GTQ</option>
-          <option value="HKD">HKD</option>
-          <option value="HRK">HRK</option>
-          <option value="HUF">HUF</option>
-          <option value="IDR">IDR</option>
-          <option value="ILS">ILS</option>
-          <option value="INR">INR</option>
-          <option value="ISK">ISK</option>
-          <option value="JPY">JPY</option>
-          <option value="KRW">KRW</option>
-          <option value="KZT">KZT</option>
-          <option value="MVR">MVR</option>
-          <option value="MXN">MXN</option>
-          <option value="MYR">MYR</option>
-          <option value="NOK">NOK</option>
-          <option value="NZD">NZD</option>
-          <option value="PAB">PAB</option>
-          <option value="PEN">PEN</option>
-          <option value="PHP">PHP</option>
-          <option value="PKR">PKR</option>
-          <option value="PLN">PLN</option>
-          <option value="PYG">PYG</option>
-          <option value="RON">RON</option>
-          <option value="RUB">RUB</option>
-          <option value="SAR">SAR</option>
-          <option value="SEK">SEK</option>
-          <option value="SGD">SGD</option>
-          <option value="THB">THB</option>
-          <option value="TRY">TRY</option>
-          <option value="TWD">TWD</option>
-          <option value="UAH">UAH</option>
-          <option value="UYU">UYU</option>
-          <option value="ZAR">ZAR</option>
+          {currencyOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
         </select>
 
         <p className="conversion-result">
